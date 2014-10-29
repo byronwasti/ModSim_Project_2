88954d1451c541041548c4c38f1c2a4d424652cb
Byron Wasti <byron.wasti@gmail.com>
2014-10-29 09:20:10 -0400

    A few changes

--- main.m
+++ main.m
@@ -6,14 +6,17 @@ function main()
     b = zeros(stop,1);  % Blood vector setup
    
     s(1) = 25;
+    b(1) = 25;
     for i = 1:stop
-        s(i+1) = s(i) - sdiff(s(i),m(i),b(i));
-        m(i+1) = m(i) - idiff(s(i),m(i),b(i));
+        s(i+1) = s(i) + sdiff(s(i),m(i),b(i));
+        m(i+1) = m(i) + idiff(s(i),m(i),b(i));
+        b(i+1) = b(i) + bdiff(s(i),m(i),b(i));
     end
     
     function res = sdiff(s,m,b)
-        alpha = .08;
-        res = s*alpha;
+%         alpha = 0.08;
+%         res = s*alpha;
+          res = 0;
     end
 
     function res = idiff(s,m,b)
@@ -21,7 +24,8 @@ function main()
     end
 
     function res = bdiff(s,m,b)
-        res = 0;
+        alpha = 0.08;
+        res = -b*alpha;
     end
         
     plotter(s,1,m,1,b,1); % Adding partial plotting functionality
