176d2d289630113f7f66a5e5fba78885d256e124
Byron Wasti <byron.wasti@gmail.com>
2014-11-04 16:31:54 -0500

    Changes to a few files
    Updated IbuprofenTime Series to represent current model of the system

--- IbuprofenTimeSeries.m
+++ IbuprofenTimeSeries.m
@@ -1,5 +1,7 @@
-function [T,M] = IbuprofenTimeSeries();
+function IbuprofenTimeSeries();
    
+    clf;
+    
     fasted = csvread('FastedDigital.csv',6,0);
     light = csvread('LightBreakfastDigital.csv',6,0);
     heavy = csvread('HeavyBreakfastDigital.csv',6,0);
@@ -7,100 +9,139 @@ function [T,M] = IbuprofenTimeSeries();
     lights = csvread('LightStomach.csv',6,0);
     heavys = csvread('HeavyStomach.csv',6,0);
     
-    y1_init = 100;  % Digestive system concentration of Ibuprofen, Bolus input
+    y1_init = 800;  % Digestive system concentration of Ibuprofen, Bolus input
                     % 800mg of Ibuprofen conecntration in tablet
     y2_init = 0;  % Blood Plasma concentration of Ibuprofen in ug/ml
+    y3_init = 0;
     
     hour = 60*60;
     
+    %=============
+    % Graphing Variables
+    axisStomach = [ -1 25 -10 800 ];
+    axisFood = [ -1 25 0 20 ];
+    axisBlood = [ -1 25 0 125 ];
+    
     % ============
     % Gastric emptying rate
-    alpha = 0.00003; % The rate at which the gastric emptying happens
+%    alpha = .0138; % The rate at which the gastric emptying happens
+    alpha = .00003
+    loss = .3 % The amount of ibuprofen lost per stock shift
+    
     
     % ============
-    % Blood Ipubrofen Concentration
-    beta = 33; % The negative perabola making parameter
-    gamma = .000015 % 
-    protein = 0 % Protein and fat parameter
+    % Food ibu. concentration
+    beta = .0033; % Food elimination rate
+    y2crit = 40;
     
-    
-    [T,M] = ode45(@deriv,[0 25*hour],[y1_init; y2_init]); 
-    hours = T/hour;
+    % ============
+    % Blood Ibuprofen Concentration
+    gamma = .00004; % Rate of eliminatio..n
     
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Plotting
     
-    % Plotting Ibu in stomach
+    % Plotting Ibu with fasting
+    pf = 0;
+    [T,M] = ode45(@deriv,[0 25*hour],[y1_init; y2_init; y3_init]); 
+    hours = T/hour;
     
-    subplot(3,2,1)
-    title('Tablet in Stomach','FontSize',14);
+    subplot(3,3,1)
+    title('Ibuprofen in Stomach','FontSize',14);
     hold all
-    plot(fasteds(:,1),fasteds(:,2),'rx','LineWidth',3);
+    plot(fasteds(:,1),fasteds(:,2)*8,'rx','LineWidth',3);
     plot(hours, M(:,1),'b','LineWidth',2);
-    xlim([0,25])
+    axis(axisStomach)
     
-    subplot(3,2,3)
-    hold all
-    plot(lights(:,1),lights(:,2),'rx','LineWidth',3);
-    plot(hours, M(:,1),'b','LineWidth',2);
-    ylabel('% Disintegration of Tablet in Stomach','FontSize',12)
-    xlim([0,25])
+    subplot(3,3,2)
+    title('Ibuprofen in Food','FontSize',14)
+    hold all;
+    plot(hours, M(:,2),'b','LineWidth',2);
+    axis(axisFood)
     
-    subplot(3,2,5)
+    subplot(3,3,3)
+    title('Ibuprofen in Blood Plasma','FontSize',14)
     hold all
-    plot(heavys(:,1),heavys(:,2),'rx','LineWidth',3);
+    plot(fasted(:,1),fasted(:,2)*5,'rx','LineWidth',3);
+    plot(hours,M(:,3),'b','LineWidth',2)
+    axis(axisBlood)
+    
+
+    
+    %=====================================
+    %Plotting Ibuprofen light breakfast
+    pf = .07;
+    
+    [T,M] = ode45(@deriv,[0 25*hour],[y1_init; y2_init; y3_init]); 
+    hours = T/hour;
+    
+    subplot(3,3,4)
+    hold all
+    plot(lights(:,1),lights(:,2)*8,'rx','LineWidth',3);
     plot(hours, M(:,1),'b','LineWidth',2);
-    xlim([0,25])
-    xlabel('Time in Hours','FontSize',12)
+    ylabel('Mg of Ibuprofen in Stomach','FontSize',12)
+    axis(axisStomach)
+
+    subplot(3,3,5)
+    plot(hours, M(:,2),'b','LineWidth',2);
+    axis(axisFood)
+    
+    subplot(3,3,6)
+    hold all
+    ylabel('Mg of Ibuprofen in Blood Plasma','FontSize',12)
+    plot(light(:,1),light(:,2)*5,'rx','LineWidth',3);
+    plot(hours,M(:,3),'b','LineWidth',2)
+    axis(axisBlood)
     
     % ====================================
-    % Plotting Ibuprof in Blod
+    % Plotting Ibuprof Heavy breakfast
+    pf = .33
     
+    [T,M] = ode45(@deriv,[0 25*hour],[y1_init; y2_init; y3_init]); 
+    hours = T/hour;
     
-    subplot(3,2,2)
-    title('Ibuprofen Concentration','FontSize',14)
+    subplot(3,3,7)
     hold all
-    plot(fasted(:,1),fasted(:,2),'rx','LineWidth',3);
-    plot(hours,M(:,2),'b','LineWidth',2)
-    xlim([0,25])
-    ylim([0,25])
+    plot(heavys(:,1),heavys(:,2)*8,'rx','LineWidth',3);
+    plot(hours, M(:,1),'b','LineWidth',2);
+    axis(axisStomach)
+    xlabel('Time in Hours','FontSize',12)
     
-    subplot(3,2,4)
+    
+    subplot(3,3,8)
+    plot(hours, M(:,2),'b','LineWidth',2);
+    xlabel('Time in Hours', 'FontSize',12);
+    axis(axisFood);
+    
+    
+    subplot(3,3,9)
     hold all
-    ylabel('Ibuprofen in Plasma (ug/ml)','FontSize',12)
-    plot(light(:,1),light(:,2),'rx','LineWidth',3);
-    plot(hours,M(:,2),'b','LineWidth',2)
-    xlim([0,25])
-    ylim([0,25])
-    
-    subplot(3,2,6)
-    hold all
-    plot(heavy(:,1),heavy(:,2),'rx','LineWidth',3);
-    
-    %jank code spot
-    protein = 0;
-    gamma = 0.00002
-    
-    [T,M] = ode45(@deriv,[0 25*hour],[y1_init; y2_init]); 
-    %end of jank code spot
-    
-    plot(hours+3,M(:,2),'b','LineWidth',2)
-    xlim([0,25])
-    ylim([0,25])
+    plot(heavy(:,1),heavy(:,2)*5,'rx','LineWidth',3);
+    plot(hours,M(:,3),'b','LineWidth',2)
+    axis(axisBlood)
     xlabel('Time in Hours','FontSize',12)
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Functions
-    
-    
+
     function res = deriv(t,Y);
         y1 = Y(1);
         y2 = Y(2);
+        y3 = Y(3);
         
-        dy1dt = -alpha*y1; % -.00138 for complete linearity slope
-        dy2dt = gamma*(y1 - beta - protein);
         
-        res = [dy1dt;dy2dt];      
+        dy1dt = -alpha*y1; % -.00138 for linear slope
+        
+%         if y1 < 0;  % Never can get negative concentrations in stomach
+%             dy1dt = 0;
+%         end
+        fddig = beta*(y2/y2crit) % Food digestion, elimnation of food
+        dy2dt = -dy1dt*(pf)*loss - fddig; 
+        
+        dy3dt = -dy1dt*(1 - pf)*loss + fddig -gamma*y3;
+        
+        
+        res = [dy1dt;dy2dt;dy3dt];      
     end  
 end
